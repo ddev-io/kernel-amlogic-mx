8077df3bd7669678a3044b5d350b2bc03c29b439
japj <japj@example.com>
2026-08-06 21:18:41 +0300

    mtd: aml_nand: include platform API for G339

--- drivers/amlogic/nand/g339_service_bbt.c
+++ drivers/amlogic/nand/g339_service_bbt.c
@@ -6,6 +6,7 @@
 #include <linux/kernel.h>
 #include <linux/mtd/mtd.h>
 #include <linux/mtd/nand.h>
+#include <linux/platform_device.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 #include <linux/slab.h>
